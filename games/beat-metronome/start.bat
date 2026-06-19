e7405c6f7a63633775315d9bb637a0a6cb3f2414
Jinhyun Baek <baekjinhyun04@gmail.com>
2026-06-19 20:39:47 +0900

    isolate BeatNet/numpy downgrade in project-local venv

--- games/beat-metronome/start.bat
+++ games/beat-metronome/start.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 echo 서버를 시작합니다...
-start "beat-metronome-server" cmd /k python server.py
+start "beat-metronome-server" cmd /k venv\Scripts\python.exe server.py
 timeout /t 3 /nobreak >nul
 start index.html
